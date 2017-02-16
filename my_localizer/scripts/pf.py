3423954ea3564b01eeab40a0ec25e7089c950519
KatieButler <katiethetraveller@gmail.com>
2017-02-16 16:06:49 -0500

    it makes the call

--- my_localizer/scripts/pf.py
+++ my_localizer/scripts/pf.py
@@ -117,9 +117,12 @@ class ParticleFilter:
         # request the map from the map server, the map should be of type nav_msgs/OccupancyGrid
         # TODO: fill in the appropriate service call here.  The resultant map should be assigned be passed
         #       into the init method for OccupancyField
-
+        rospy.wait_for_service('static_map')
+        try:
+            map_proxy = rospy.ServiceProxy('static_map', GetMap)
+            my_map = map_proxy().map
         # for now we have commented out the occupancy field initialization until you can successfully fetch the map
-        #self.occupancy_field = OccupancyField(map)
+        self.occupancy_field = OccupancyField(my_map)
         self.initialized = True
 
     def update_robot_pose(self):
