2b8e68d548ec5b28bae082f089d49319d930351b
dhash <dhash@Supernova-2ML-DE5450>
2017-02-17 04:38:31 -0500

    Code style TODO 1 TODO down

--- my_localizer/scripts/pf.py
+++ my_localizer/scripts/pf.py
@@ -115,12 +115,11 @@ class ParticleFilter:
         self.current_odom_xy_theta = []
 
         # request the map from the map server, the map should be of type nav_msgs/OccupancyGrid
-        # TODO: fill in the appropriate service call here.  The resultant map should be assigned be passed
-        #       into the init method for OccupancyField
         rospy.wait_for_service('static_map')
         try:
             map_proxy = rospy.ServiceProxy('static_map', GetMap)
             my_map = map_proxy().map
+        #TODO: uncaught except
         # for now we have commented out the occupancy field initialization until you can successfully fetch the map
         self.occupancy_field = OccupancyField(my_map)
         self.initialized = True
